9b77a10ad9abf955cf6853a0c8f15f41e9901dab
D'elegance Lane <delegancelane@gmail.com>
2021-01-02 18:09:13 -0600

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Westside Health Authority on 1/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
