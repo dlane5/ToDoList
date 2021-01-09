37df9610c92a08794ef5a621c038448c485815f4
D'elegance Lane <delegancelane@gmail.com>
2021-01-08 20:22:59 -0600

    2page app
    TodoList

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,35 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Westside Health Authority on 1/2/21.
+//
+
+import Foundation
+
+import UIKit
+
+
+struct ToDo {
+ var title: String
+ var isComplete: Bool
+ var dueDate: Date
+ var notes: String?
+    
+
+
+ static func loadToDos() -> [ToDo]? {
+return nil
+}
+ static func loadSampleToDos() -> [ToDo] {
+    let todo1 = ToDo(title: "ToDo One", isComplete: false,
+    dueDate: Date(), notes: "Notes 1")
+    let todo2 = ToDo(title: "ToDo Two", isComplete: false, dueDate: Date(), notes: "Notes 2")
+    let todo3 = ToDo(title: "ToDo Three", isComplete: false,
+    dueDate: Date(), notes: "Notes 3")
+
+    return [todo1, todo2, todo3]
+ }
+
+}
+
---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Westside Health Authority on 1/2/21.
+//
+
+import Foundation
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    var todos = [ToDo]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        if let savedToDos = ToDo.loadToDos() {
+            todos = savedToDos
+        } else {
+            todos = ToDo.loadSampleToDos()
+        }
+    }
+  
+
+
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return todos.count
+    }
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier:
+  "ToDoCellIdentifier", for: indexPath)
+
+   let todo = todos[indexPath.row]
+    cell.textLabel?.text = todo.title
+   return cell
+}
+
+
+}
